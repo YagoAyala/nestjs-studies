72633c7ee24e47fd2a685809e77639fe68535838
YagoAyala <yago.ayala@outlook.com>
2021-09-04 11:37:46 -0300

    feat(entity): createad an entity on the database

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column()
   size: number;
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
-@Module({ controllers: [ProductsController], providers: [ProductsService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Product])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
 export class ProductsModule {}
