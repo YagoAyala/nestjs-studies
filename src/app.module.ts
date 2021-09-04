95a1afca1149d487cc8f7dd0c4af0bddb4396430
YagoAyala <yago.ayala@outlook.com>
2021-09-04 11:37:21 -0300

    feat(database): created a database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
